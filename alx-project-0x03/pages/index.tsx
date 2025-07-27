3ebfe3afa5adcf1b91aed7c230a6d4aa9d1f3c5e
el-ahmdo <aliyua875@gmail.com>
2025-07-27 18:54:12 +0100

    second commit

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,16 +1,9 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+const Home = () => {
+  return (
+    <div>
+      <h1> Welcome to Splash App </h1>
+    </div>
+  );
+};
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function Home() {
-  return <></>;
-}
+export default Home;
